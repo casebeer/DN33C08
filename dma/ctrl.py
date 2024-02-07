7d4738c0937ddb0b3965fc73b596c8814df56dff
Christopher H. Casebeer <christopher@chc.name>
2024-02-07 06:14:40 -0500

    Cleanups of experimental register wrapping classes

--- dma/ctrl.py
+++ dma/ctrl.py
@@ -1,9 +1,6 @@
 
 #from ctypes import c_uint32 as uint32
 
-def uint32(value):
-  return 0xffffffff & value
-
 class Ctrl():
   '''
   Set up a CTRL byte to configure a DMA channel
@@ -87,86 +84,146 @@ class Ctrl():
 
     return config
 
+def uint32(value):
+  return 0xffffffff & value
+
 class RegisterField():
   '''
   Metadata and access helpers for fields within 32-bit bitfield registers
   '''
-  def __init__(self, bit_position:int, bit_size:int=1, _type:type=int):
+  def __init__(self, bit_position:int, bit_size:int=1, type_:type=int):
     self.bit_position = uint32(bit_position)
     self.bit_size = uint32(bit_size)
     self.public_name = None
-    self._type = _type
+    self.type_ = type_
   def bitmask(self):
     '''Bitmask for this field'''
     return uint32(~(0xfffffffe << (self.bit_size - 1)) << self.bit_position)
   def __set_name__(self, owner, name):
     self.public_name = name
-    #self.private_name = f"_{name}"
   def __get__(self, obj, objtype=None):
-    #return getattr(obj, self.private_name)
-    return self._type((obj.register_value & self.bitmask()) >> self.bit_position)
+    return self.type_((obj.register_value & self.bitmask()) >> self.bit_position)
   def __set__(self, obj, value):
-    #setattr(obj, self.private_name, value)
     obj.register_value &= ~self.bitmask()
     obj.register_value |= (uint32(value) << self.bit_position) & self.bitmask()
   def __repr__(self):
-    return f"RegisterField({self.bit_size} bit {self._type.__name__} @ bit {self.bit_position})"
+    return f"RegisterField({self.bit_size} bit {self.type_.__name__} @ bit {self.bit_position})"
 
-class CtrlRegister():
-  '''
-  Wrapper for bitfields within the DMA CTRL registers
-  '''
-  busy = RegisterField(24, _type=bool)
-  sniff_en = RegisterField(23, _type=bool)
-  bswap = RegisterField(22, _type=bool)
-  irq_quiet = RegisterField(21, _type=bool)
-  treq_sel = RegisterField(15, 6)
-  chain_to = RegisterField(11, 4)
-  ring_sel = RegisterField(10, _type=bool)
-  ring_size = RegisterField(6, 4)
-  incr_write = RegisterField(5, _type=bool)
-  incr_read = RegisterField(4, _type=bool)
-  data_size = RegisterField(2, 2)
-  high_priority = RegisterField(1, _type=bool)
-  en = RegisterField(0, _type=bool)
-  def __init__(self, register_value=0):
-    self.register_value = uint32(register_value)
+class Alias():
+  def __init__(self, target):
+    self.target = target
+    self.name = None
+  def __set_name__(self, owner, name):
+    self.name = name
+  def __get__(self, obj, objtype=None):
+    return getattr(obj, self.target)
+  def __set__(self, obj, value):
+    setattr(obj, self.target, value)
+  def __repr__(self):
+    return f"AliasedRegisterField({self.name} => {self.target})"
 
-class CtrlBuilder():
-  '''
-  Chainable helper to set raw DMA CTRL register fields
-  '''
-  def __init__(self, register_value):
-    self.register = CtrlRegister(register_value)
-  def value(self):
-    return uint32(self.register.register_value)
+class BitfieldRegister():
+  DEFAULT_REGISTER_VALUE = 0
+  def __init__(self, register_value=None):
+    if register_value is None:
+      self.register_value = self.DEFAULT_REGISTER_VALUE
+    else:
+      self.register_value = uint32(register_value)
+
   def fields(self):
-    for attr, value in vars(type(self.register)).items():
-      if isinstance(value, RegisterField):
+    '''Return the names of the register fields wrapped by this object'''
+    for attr, value in vars(type(self)).items():
+      if isinstance(value, RegisterField) or isinstance(value, Alias):
         yield attr
-  def make_setter(self, name):
-    def setter(value):
+
+  def value(self):
+    '''Return the value of the register as a uint32'''
+    return uint32(self.register_value)
+
+def not_implemented(*args, **kwargs):
+  '''Helper to throw errors for abstract base classes'''
+  raise NotImplementedError()
+
+class RegisterBuilder():
+  '''Abstract base class for chainable register builder helpers'''
+  register_class = not_implemented # must override
+  def __init__(self, register_value=None):
+    self.register = self.register_class(register_value)
+  def value(self):
+    '''Return the value of the register as a uint32'''
+    return uint32(self.register.value())
+  def _make_setter(self, name):
+    def setter(value=None):
+      if value is None:
+        # act as getter not setter
+        return getattr(self.register, name)
       setattr(self.register, name, value)
       # support chaining API
       return self
     return setter
   def __getattr__(self, name):
-    if name in self.fields():
-      #return getattr(self.register, name)
-      return self.make_setter(name)
+    if name in self.register.fields():
+      return self._make_setter(name)
     raise AttributeError(f"'{type(self).__name__}' object has no attribute '{name}'")
 
+class CtrlRegister(BitfieldRegister):
+  '''
+  Wrapper for bitfields within the DMA CTRL registers
+  '''
+  busy = RegisterField(24, type_=bool)
+  sniff_en = RegisterField(23, type_=bool)
+  bswap = RegisterField(22, type_=bool)
+  irq_quiet = RegisterField(21, type_=bool)
+  treq_sel = RegisterField(15, 6)
+  chain_to = RegisterField(11, 4)
+  ring_sel = RegisterField(10, type_=bool) # False => wrap reads; True => wrap writes
+  ring_size = RegisterField(6, 4)
+  incr_write = RegisterField(5, type_=bool)
+  incr_read = RegisterField(4, type_=bool)
+  data_size = RegisterField(2, 2)
+  high_priority = RegisterField(1, type_=bool)
+  en = RegisterField(0, type_=bool)
+
+  enable = Alias('en')
+  wrap_writes = Alias('ring_sel')
+
+  # TODO: Set defaults
+  DEFAULT_REGISTER_VALUE = 0
+
+class CtrlBuilder(RegisterBuilder):
+  '''
+  Chainable helper to set raw DMA CTRL register fields
+  '''
+  register_class = CtrlRegister
+
+
+'''
+ctrl_builder = CtrlBuilder()\
+                 .treq_sel(dma.addresses.Dreq().Pio(0).tx_fifo(0))\
+                 .irq_quiet(False)\
+                 .incr_write(False)\
+                 .incr_read(True)\
+                 .ring_sel(False)\
+                 .ring_size(4)\
+                 .data_size(2)\
+                 .en(True)
+'''
+
 #from pprint import pprint
-#foo = RegisterField(24, _type=bool)
+#foo = RegisterField(24, type_=bool)
 #print(f'{foo.bitmask():32b}')
 #bar = RegisterField(15, 6)
 #print(f'{bar.bitmask():32b}')
 #
 #c = CtrlBuilder()
 #pprint(type(c.register).__dict__)
-#pprint(list(c.fields()))
-#pprint('treq_sel' in c.fields())
+##pprint(list(c.fields()))
+##pprint('treq_sel' in c.fields())
 #pprint(c.treq_sel)
+#pprint(c.enable())
+#pprint(c.enable(True).enable())
+#
+#pprint(f"{c.ring_size(7).enable(True).incr_read(True).value():32b}")
 #
 #import code
 #code.interact(local=locals())
